23f4664e6eef472a6210d3bcbeeb196acf52fb61
Truston Ailende <trustonailende@gmail.com>
2018-09-15 21:06:13 +0100

    Create Indices.py

---
+++ Lekki/Indices.py
@@ -0,0 +1,23 @@
+#Name:Iteoluwa Oladejo
+#Email:iteoluwa.oladejo@gmail.com
+
+input("This is a program about indices")
+
+input("This program only utilizes the multiplication law of indices")
+
+input("Please enter only whole numbers for both the standing numbers and the powers")
+
+a=int(input("Please enter a first number:"))
+
+b=int(input("Please enter a second number of your choice:"))
+
+x=int(input("Please enter a power for the first standing number:"))
+
+y=int(input("Please enter a power for the second standing number:"))
+
+indice=a**x*b**y
+a**x+y
+result=indice**x+y
+
+
+print(indice)
