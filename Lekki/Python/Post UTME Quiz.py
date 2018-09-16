2ee52ea7772005e25ba2089803cf135e83bf655c
Truston Ailende <trustonailende@gmail.com>
2018-09-16 16:33:14 +0100

    Create Post UTME Quiz.py

---
+++ Lekki/Python/Post UTME Quiz.py
@@ -0,0 +1,64 @@
+# Name: Dare Coker
+# Email dareycoker819@yahoo.com
+
+print("1,     My brother, Gabriel,recevied #230 in __________ from Nigerpools? (a)winnings\n (b)a winning\n (c)the winning\n (d) winning\n")
+ans1 = input("enter your answer a,b,c,d,:")
+score = 0
+if (ans1 == "b" or ans1 == "B"):
+     print("correct")
+     score=score + 1
+     print("you have" , score, "point")
+else:
+    print ("incorrect")
+           
+#using or incase of caps
+print("2,     __________ people now go to herbalists for the treatment of malari. (a)Not some\n (b)Not much \n(c)Few \n(d)A little\n")
+ans2 = input("enter your answer a,b,c,d,:")
+score = 0
+if (ans1 == "c" or ans1 == "c"):
+     print("correct")
+     score=score + 1
+     print("you have" , score, "point")
+else:
+    print ("incorrect")
+    #using or incase of caps
+print ("3.    A teacher [distributing paper herself]:'could______________take________ paper and use_____ paints from ___ shelf there.(a)each of you ... that ... these ... this\n(b)each of you ... this ... those ... that\n(c)you of each ..that ... these ... this\n(d)every you ... this ... those ... that\n")
+ans3 = input("enter your answer a,b,c,d,:")
+score = 0
+if (ans1 == "c" or ans1 == "c"):
+     print("correct")
+     score=score + 1
+     print("you have" , score, "point")
+else:
+    print ("incorrect")
+       #using or incase of caps
+print("4.    __________ helpers received _______ credit,  although _____ deserved _______ praise. (a)Half the ... much ... them of all ... little\n(b)Half ... much ... all them ... much\n(c)Half the ... a great deal of ... all of them ... a lot of \n(d)The half ... a deal of ... them all .. much\n")
+ans4 = input("enter your answer a,b,c,d,:")
+score = 0
+if (ans1 == "b" or ans1 == "B"):
+     print("correct")
+     score=score + 1
+     print("you have" , score, "point")
+else:
+    print ("incorrect")
+    #using or incase of caps
+print("5.   In mose Ngerian village there is _____ of food.(a)much\n(b)plenty\n (c)some\n (d)enough\n")
+ans5 = input("enter your answer a,b,c,d,:")
+score = 0
+if (ans1 == "b" or ans1 == "B"):
+     print("correct")
+     score=score + 1
+     print("you have" , score, "point")
+else:
+    print ("incorrect")
+     #using or incase of caps  
+print ("6.      MY friend, Kola, is looking for ___________ in lagos.(a)job\n (b)the job \n(c)a job\n(d)the jobs\n")
+ans6 = input("enter your answer a,b,c,d,:")
+score = 0
+if (ans1 == "c" or ans1 == "c"):
+     print("correct")
+     score=score + 1
+     print("you have" , score, "point")
+else:
+    print ("incorrect")
+    print ("your total is " , score,"point") 
