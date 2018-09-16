31444382231d79db742239935aa5e6af8fc052ee
Truston Ailende <trustonailende@gmail.com>
2018-09-16 15:28:11 +0100

    Create Quadratic Equations.py

---
+++ Lekki/Python/Quadratic Equations.py
@@ -0,0 +1,27 @@
+# Name:OGUNDERO CHRISTOPHER G
+#Email: chrisblue006@yahoo.com
+
+# Solve the quadratic equation ax**2 + bx + c = 0
+
+# import complex math module
+import cmath
+
+#note: you can change the value here
+
+a = 1
+b = 8
+c = 4
+
+# To take coefficient input from the users
+# a = float(input('Enter a: '))
+# b = float(input('Enter b: '))
+# c = float(input('Enter c: '))
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1,sol2))
