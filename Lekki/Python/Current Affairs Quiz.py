de6ff0e503e1a30bdfb83f5db3c484fbdb641a91
Truston Ailende <trustonailende@gmail.com>
2018-09-16 15:24:31 +0100

    Create Current Affairs Quiz.py

---
+++ Lekki/Python/Current Affairs Quiz.py
@@ -0,0 +1,114 @@
+#CodeLagos
+#ABDULLAHI YAYA MAIMUNATU. maimunatuabdulahi@yahoo.com
+
+#Display the instruction
+print("Select the correct option from (a,b,c,d)")
+
+#Display Current Affairs
+print("CURRENT AFFAIRS")
+ 
+print("(1)Who is the current INEC chairman?\n(a)Attahiru Jega (b)Yakubu Maikudi (c)Mahmood Yakubu (d)Labaran Sabo : ")
+sol1=input("Enter your answer a,b,c,d:")
+score=0
+answer1 = "Correct"
+answer2 = "Wrong"
+
+if(sol1 == 'c'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(2)Who is the current NAFDAC Director General? \n(a)Mojisola Christiana Adeyeye (b)Grace Akindele (c)Maryam Badmus (d)Kelechi Ugo: ")
+sol2=input("Enter your answer a,b,c,d :")
+
+if(sol2 == 'a'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(3)What does the Eagle in the Nigerian coat of arm represent? \n(a)Peace (b)Strength (c)Dignity (d)Power: ")
+sol3=input("Enter your answer a,b,c,d :")
+
+if(sol3 == 'b'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(4)Who is the current Finance Minister? \n(a)Kemi Adeosun (b)Zainab Ahmed (c)Aisha Bello (d)Zainab Adamu : ")
+sol4=input("Enter your answer a,b,c,d:")
+
+if(sol4 == 'b'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(5)One of this was in existence before the outbreak of the second World War? \n(a)The league of Nations (b)The UNO (c)The Commonwealth of Nations (d)ECOWAS:  ")
+sol5=input("Enter your answer a,b,c,d:")
+
+if(sol5 == 'a'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(6)Nigeria got her independence in what year? \n(a)1970 (b)1960 (c)1980 (d)1950: ")
+sol6=input("Enter your answer a,b,c,d:")
+score=0
+answer1 = "Correct"
+answer2 = "Wrong"
+
+if(sol6 == 'b'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(7)Which state in Nigeria has the highest Local Goverment Area? \n(a)Lagos (b)Niger (c)Kano (d)Edo: ")
+sol7=input("Enter your answer a,b,c,d:")
+
+if(sol7 == 'c'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(8)Which state in Nigeria is the food basket of the nation? \n(a)Benue (b)Enugu (c)Kaduna (d)Ogun: ")
+sol8=input("Enter your answer a,b,c,d:")
+
+if(sol8 == 'a'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(9)A bill that applies to the whole population and is intended to promote the general welfare is called? \n(a)A private bill (b)A decree (c)An Appropriation bill (d)A public bill: ")
+sol9=input("Enter your answer a,b,c,d:")
+
+if(sol9 == 'c'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+print("(10)Who was the first Military Head of State in Nigeria? \n(a)Gen. Ibrahim Babangida (b)Gen. Murtala Mohammed (c)Gen. Thomas Aguiyi-Ironsi (d)Chief Olusegun Obasanjo:  ")
+sol10=input("Enter your answer a,b,c,d:")
+
+if(sol10 == 'c'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+print(score)
